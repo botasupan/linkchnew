f95d2b02af9f624374b69b2cc248d08c975a17ee
botasupan <87362466+botasupan@users.noreply.github.com>
2021-11-11 18:22:17 +0700

    Add files via upload

---
+++ plugins/cbb.py
@@ -0,0 +1,28 @@
+#(©)Codexbotz
+
+from pyrogram import __version__
+from bot import Bot
+from config import OWNER_ID
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
+
+@Bot.on_callback_query()
+async def cb_handler(client: Bot, query: CallbackQuery):
+    data = query.data
+    if data == "about":
+        await query.message.edit_text(
+            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Support : <a href='https://t.me/asupanindoviraals'>Channel</a>",
+            disable_web_page_preview = True,
+            reply_markup = InlineKeyboardMarkup(
+                [
+                    [
+                        InlineKeyboardButton("🔒 ᴄʟᴏsᴇ", callback_data = "close")
+                    ]
+                ]
+            )
+        )
+    elif data == "close":
+        await query.message.delete()
+        try:
+            await query.message.reply_to_message.delete()
+        except:
+            pass
