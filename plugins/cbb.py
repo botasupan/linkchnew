fdc70ffed2f3e21cb8efc47a82056708bb16aec3
botasupan <87362466+botasupan@users.noreply.github.com>
2021-11-11 19:12:44 +0700

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>✰ Owner : <a href='tg://user?id={OWNER_ID}'>This Person</a>",
+            text = f"<b>✰ Owner : <a href='tg://user?id={OWNER_ID}'>OWN Channel</a>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
